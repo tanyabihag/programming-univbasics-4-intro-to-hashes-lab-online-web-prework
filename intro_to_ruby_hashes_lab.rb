3bd980584a7aff99ea20751c287b493c5b5ec88e
Tanya Bihag <tbihag@gmail.com>
2019-08-08 21:45:34 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,29 +1,49 @@
-def new_hash
-  # return an empty hash
+def new_hash = {
+  name : "Sam",
+  age : 31
+}
+  new_hash[:gender]
 end
 
-def my_hash
-  # return a valid hash with any key/value pair of your choice
+def my_hash = {
+    name: "Sam"
+    age: 10 
+}
+ my_hash[:name]
 end
 
-def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+def pioneer = {
+  name: "Grace Hopper"
+}
+  pioneer[:name]
 end
 
-def id_generator
-  # return a hash with a key :id assigned to positive integer
+def id_generator = {
+  id: 2 
+}
+id_generator[:id]
+ 
 end
 
-def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+def my_hash_creator = {
+  key: "key",
+  value: "value"
+}
+ my_hash_creator 
 end
 
-def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+def read_from_hash = {
+  hash: "key"
+  key:
+}
+  read_from_hash
 end
 
-def update_counting_hash(hash, key)
-  # given a hash an a key as parameters, return an updated hash
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
-  # if the provided key is present, increment its value by 1
+def update_counting_hash = { 
+  "hash" => 12,
+  "tanya" => 20
+}
+update_counting_hash["tanya"] += 1
+update_counting_hash["cookie"] =+ 1
+ 
 end
